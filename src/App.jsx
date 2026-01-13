4a582ef5ac33f1dd691538c5a2f24f25817e6cf4
alinapshn <alinapshenichnaya06@gmail.com>
2026-01-13 18:16:27 +0100

    create HomePage as own element

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { HomePage } from "./assets/pages/home/HomePage";
 import "./App.css";
 
 function App() {
@@ -8,115 +9,7 @@ function App() {
 
   return (
     <>
-      <header className="header">
-        <div className="header__top">
-          <div className="header__wrapper container">
-            <div className="header__top-location">
-              <img src="./img/icons/map-pin.svg" alt="" />
-              <p>Store Location: Lincoln- 344, Illinois, Chicago, USA</p>
-            </div>
-
-            <div className="header__user">
-              <button className="header__user-lang">
-                Eng <img src="./img/icons/arrow-down.svg" />
-              </button>
-              <button className="header__user-currency">
-                USD <img src="./img/icons/arrow-down.svg" />
-              </button>
-              <div className="header__user-auth">
-                <a href="#" className="header__link">
-                  Sign In /
-                </a>
-                <a href="#" className="header__link">
-                  Sign Up
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="header__middle">
-          <div className="header__wrapper container">
-            <div className="header__middle-logo">
-              <img src="./img/icons/logo.svg" />
-              <h5>Ecobazar</h5>
-            </div>
-            <div className="header__search">
-              <div className="header__search-input">
-                <img src="./img/icons/search.svg" />
-                <input type="text" placeholder="Search" />
-              </div>
-              <button className="header__search-button">Search</button>
-            </div>
-            <div className="header__middle-buttons">
-              <button className="header__middle-fav">
-                <img src="./img/icons/favorites.svg" />
-              </button>
-              <button className="header__cart">
-                <div className="header__cart-icon">
-                  <img src="./img/icons/cart.svg" />
-                  <div className="header__cart-quantity">2</div>
-                </div>
-
-                <div className="header__cart-text">
-                  <p>Shopping cart:</p>
-                  <p>$57.00</p>
-                </div>
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div className="header__bottom">
-          <div className="header__wrapper container">
-            <div className="header__bottom-nav">
-              <button className="header__bottom-menu">
-                <img src="./img/icons/menu.svg" />
-                <p>All Categories</p>
-                <img src="./img/icons/arrow-down.svg" />
-              </button>
-
-              <ul className="header__list">
-                <li>
-                  <a href="" className="header__list-item">
-                    Home <img src="./img/icons/arrow-down.svg" />
-                  </a>
-                </li>
-                <li>
-                  <a href="" className="header__list-item">
-                    Shop <img src="./img/icons/arrow-down.svg" />
-                  </a>
-                </li>
-                <li>
-                  <a href="" className="header__list-item">
-                    Pages <img src="./img/icons/arrow-down.svg" />
-                  </a>
-                </li>
-                <li>
-                  <a href="" className="header__list-item">
-                    Blog <img src="./img/icons/arrow-down.svg" />
-                  </a>
-                </li>
-                <li>
-                  <a href="" className="header__list-item">
-                    About Us{" "}
-                  </a>
-                </li>
-                <li>
-                  <a href="" className="header__list-item">
-                    Contact Us{" "}
-                  </a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="header__bottom-phone">
-              <img src="./img/icons/phone.svg" />
-              <p>(219) 555-0114</p>
-            </div>
-          </div>
-        </div>
-      </header>
+      <HomePage />
 
       <section className="props">
         <div className="container">
