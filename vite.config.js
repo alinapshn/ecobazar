fe734d5ad9343247dc6a490eace5bcddbb3779c8
alinapshn <alinapshenichnaya06@gmail.com>
2026-01-13 17:39:45 +0100

    fix scss imports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "/src/assets/styles/base/vars" as *;
+          @use "/src/assets/styles/base/mixins" as *;
+        `,
+      },
+    },
+  },
+});
