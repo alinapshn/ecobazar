124d09386a7f75dce5a2b4744460b2005d4a8ecd
alinapshn <alinapshenichnaya06@gmail.com>
2026-01-13 18:27:51 +0100

    fix paths & created folders for future pages

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/Header";
+import { Props } from "./Props";
+
+export function HomePage() {
+  return (
+    <>
+      <Header />
+      <Props />
+    </>
+  );
+}
