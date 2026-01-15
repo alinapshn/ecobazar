312c38e196d890d59aff5ed8015b7592d1cc625f
alinapshn <alinapshenichnaya06@gmail.com>
2026-01-15 16:30:04 +0100

    create IntroProducts component

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,13 @@
 import { Header } from "../../components/Header";
 import { Props } from "./Props";
+import { IntroProducts } from "./IntroProducts";
 
 export function HomePage() {
   return (
     <>
       <Header />
       <Props />
+      <IntroProducts />
     </>
   );
 }
