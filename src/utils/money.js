d36b129d44afae8b914301b315644cd6767e43b1
alinapshn <alinapshenichnaya06@gmail.com>
2026-01-15 17:35:35 +0100

    create function for calculate price in dollars instead of cents

---
+++ src/utils/money.js
@@ -0,0 +1,7 @@
+export function formatMoney(priceCents) {
+  if (priceCents < 0) {
+    priceCents = priceCents * -1;
+    return `-$${(priceCents / 100).toFixed(2)}`;
+  }
+  return `$${(priceCents / 100).toFixed(2)}`;
+}
