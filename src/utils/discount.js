0f4ddd815f28a37fc3a78e8c388bf484b1432484
alinapshn <alinapshenichnaya06@gmail.com>
2026-01-15 17:33:01 +0100

    create function for calculate precent of discount

---
+++ src/utils/discount.js
@@ -0,0 +1,8 @@
+export function calcDiscount(priceCents, discountPriceCents) {
+  if (!discountPriceCents || discountPriceCents >= priceCents) {
+    return null;
+  }
+
+  const discount = ((priceCents - discountPriceCents) / priceCents) * 100;
+  return Math.round(discount);
+}
