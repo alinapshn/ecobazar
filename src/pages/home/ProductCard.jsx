be278674eaf5a5c4bd18a80997bc8c8043fd44dd
alinapshn <alinapshenichnaya06@gmail.com>
2026-01-29 16:59:31 +0100

    create hover class and styles for quick view and favorite buttons

--- src/pages/home/ProductCard.jsx
+++ src/pages/home/ProductCard.jsx
@@ -2,11 +2,21 @@ import { calcDiscount } from "../../utils/discount.js";
 import { formatCurrency } from "../../utils/money.js";
 import RatingIcon from "../../assets/img/icons/rating.svg";
 import CartIcon from "../../assets/img/icons/cart.svg";
+import QuickViewIcon from "../../assets/img/icons/quick-view.svg";
+import FavoritesIcon from "../../assets/img/icons/favorites.svg";
 import "./productCard.scss";
 
 export function ProductCard({ product, variant }) {
   return (
     <div className={`product-card product-card--${variant}`}>
+      <div className={`product-card__actions product-card__actions--${variant}`}>
+        <button className={`product-card__actions-button product-card__actions-button--${variant}`}>
+          <img src={FavoritesIcon} />
+        </button>
+        <button className={`product-card__actions-button product-card__actions-button--${variant}`}>
+          <img src={QuickViewIcon} />
+        </button>
+      </div>
       {product.discountPriceCents ? (
         <p className={`product-card__sale product-card__sale--${variant}`}>
           Sale{" "}
@@ -17,9 +27,17 @@ export function ProductCard({ product, variant }) {
       ) : null}
       <img src={product.image} />
       <div className={`product-card__info product-card__info--${variant}`}>
-        <div className={`product-card__info-text product-card__info-text--${variant}`}>
-          <p className={`product-card__info-name product-card__info-name--${variant}`}>{product.name}</p>
-          <p className={`product-card__info-price product-card__info-price--${variant}`}>
+        <div
+          className={`product-card__info-text product-card__info-text--${variant}`}
+        >
+          <p
+            className={`product-card__info-name product-card__info-name--${variant}`}
+          >
+            {product.name}
+          </p>
+          <p
+            className={`product-card__info-price product-card__info-price--${variant}`}
+          >
             {product.discountPriceCents ? (
               <>
                 {formatCurrency(product.discountPriceCents)}{" "}
@@ -29,9 +47,14 @@ export function ProductCard({ product, variant }) {
               `${formatCurrency(product.priceCents)}`
             )}
           </p>
-          <img className={`product-card__info-rating product-card__info-rating--${variant}`} src={RatingIcon} />
+          <img
+            className={`product-card__info-rating product-card__info-rating--${variant}`}
+            src={RatingIcon}
+          />
         </div>
-        <button className={`product-card__info-cart product-card__info-cart--${variant}`}>
+        <button
+          className={`product-card__info-cart product-card__info-cart--${variant}`}
+        >
           <img src={CartIcon} />
         </button>
       </div>
