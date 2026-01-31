7ce232f4ac6b1a98d9b63cea7247567c9338886c
alinapshn <alinapshenichnaya06@gmail.com>
2026-01-31 13:12:47 +0100

    create script for reviews & move icons

---
+++ data/review.js
@@ -0,0 +1,34 @@
+export const reviews = [
+  {
+    id: 1,
+    author: "Kristin Watson",
+    avatar: "/images/reviews/feedback-1.jpg",
+    date: "2 min ago",
+    rating: "/images/reviews/rating.svg",
+    text: "Duis at ullamcorper nulla, eu dictum eros.",
+  },
+  {
+    id: 2,
+    author: "Jane Cooper",
+    avatar: "/images/reviews/feedback-2.jpg",
+    date: "30 Apr, 2021",
+    rating: "/images/reviews/rating.svg",
+    text: 'Keep the soil evenly moist for the healthiest growth. If the sun gets too hot, Chinese cabbage tends to "bolt" or go to seed; in long periods of heat, some kind of shade may be helpful. Watch out for snails, as they will harm the plants.',
+  },
+  {
+    id: 3,
+    author: "Jacob Jones",
+    avatar: "/images/reviews/feedback-3.jpg",
+    date: "2 min ago",
+    rating: "/images/reviews/rating.svg",
+    text: " Vivamus eget euismod magna. Nam sed lacinia nibh, et lacinia lacus.",
+  },
+  {
+    id: 4,
+    author: "Ralph Edwards",
+    avatar: "/images/reviews/feedback-4.jpg",
+    date: "2 min ago",
+    rating: "/images/reviews/rating.svg",
+    text: "200+ Canton Pak Choi Bok Choy Chinese Cabbage Seeds Heirloom Non-GMO Productive Brassica rapa VAR. chinensis, a.k.a. Canton's Choice, Bok Choi, from USA",
+  },
+];
