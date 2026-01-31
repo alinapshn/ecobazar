e5dcb0b39a4f1e3097ec07192b51f8582101ef4b
alinapshn <alinapshenichnaya06@gmail.com>
2026-01-31 16:48:31 +0100

    add ScrollToTop component to reset scroll on route change

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router';
-import App from './App.jsx'
-import './main.scss';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import { ScrollToTop } from "./components/ScrollToTop.jsx";
+import App from "./App.jsx";
+import "./main.scss";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>,
-)
+);
