9a2def53abc9e7194ee5e9c2d8abc3db2db2f8de
alinapshn <alinapshenichnaya06@gmail.com>
2026-02-21 20:50:20 +0100

    add blog sidebar section

--- src/pages/single-blog-page/SingleBlogPage.jsx
+++ src/pages/single-blog-page/SingleBlogPage.jsx
@@ -1,16 +1,30 @@
 import { useParams } from "react-router-dom";
-import { getNewsById } from "../../../data/blog";
+import {
+  getNewsById,
+  getRecentNews,
+  allTags,
+  categoriesList,
+} from "../../../data/blog";
 import { BlogInfo } from "./BlogInfo";
+import { BlogSidebar } from "../../components/BlogSidebar";
 
 export function SingleBlogPage() {
   const { blogId } = useParams();
   const post = getNewsById(blogId);
+
+  const recentPosts = getRecentNews(3);
 
   return (
     <section className="blog">
       <div className="container">
         <div className="blog__wrapper">
           <BlogInfo post={post} />
+
+          <BlogSidebar
+            categories={categoriesList}
+            tags={allTags}
+            recentPosts={recentPosts}
+          />
         </div>
       </div>
     </section>
